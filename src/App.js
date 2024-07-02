017c1bee254b35b49a15faf77370c42976e9928b
Monica Riegel <monicariegel@hotmail.com>
2024-07-02 20:57:21 -0300

    Importando Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Inicio from "./paginas/inicio";
+import SobreMim from "./paginas/sobreMim";
+
 function App() {
   return (
-    <h1>Olá Mundo</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="*" element={<div>Pagina não encontrada</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
